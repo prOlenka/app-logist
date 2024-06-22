11121c3ab2f3a1d1ef464ee600ade53680a450bb
Pronina <olia.prosha@gmail.com>
2024-06-22 09:28:47 +0300

    delete KafkaConfig

--- src/main/java/com/intership/app_logist/LogistServiceApplication.java
+++ src/main/java/com/intership/app_logist/LogistServiceApplication.java
@@ -3,10 +3,9 @@ package com.intership.app_logist;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration;
-import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
+
 
 @SpringBootApplication (exclude = {R2dbcAutoConfiguration.class})
-//@EnableR2dbcRepositories(basePackages = "com.intership.app_logist.repository")
 public class LogistServiceApplication {
 
 	public static void main(String[] args) {
